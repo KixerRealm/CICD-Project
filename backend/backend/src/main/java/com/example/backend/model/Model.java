2f0ccd73e7f858ec439b638deadebeb992bc3fad
Kristijan Kostovski <kixer.wade2000@outlook.com>
2023-06-25 15:40:59 +0200

    Dockerize backend and database

--- backend/backend/src/main/java/com/example/backend/model/Model.java
+++ backend/backend/src/main/java/com/example/backend/model/Model.java
@@ -1,10 +1,11 @@
 package com.example.backend.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Data
 @NoArgsConstructor
